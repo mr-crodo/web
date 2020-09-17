19f44ee51c9736c1d478efdfb67e4a9a08160ac0
Nasib <bduti09@gmail.com>
2020-09-17 20:46:28 +0400

    js lesson 9

---
+++ JS-lesson/assets/js/main.js
@@ -0,0 +1,27 @@
+// Comment
+"use strict";
+var setMir = "Hello Nasib";
+console.log("setMir", setMir);
+
+function nameT () {
+  let nasIb = "Hi baby";
+  console.log("nameT -> nasIb", nasIb);
+}
+
+// name();
+
+function lastname () {
+  const nasIb = "Hello World";
+  console.log("lastname -> nasIb", nasIb);
+  var setMir = "AUS";
+  console.log("lastname -> setMir", setMir);
+}
+
+
+
+nameT();
+lastname();
+
+// console.log(nameT() + lastname());
+
+console.log("eto proverka", setMir);
