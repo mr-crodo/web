d3ab32985f1b7406e6b8d04db1ad91b166c60fc4
Nasib <bduti09@gmail.com>
2020-10-03 23:19:07 +0400

    Function JS

--- JS-lesson/assets/js/main.js
+++ JS-lesson/assets/js/main.js
@@ -1,19 +1,25 @@
-// Comment
-// "use strict";
+"use strict";
 
 
-let answer = [],
-  questions = [
-    'What is you name?',
-    'What is you sure name?',
-    'What is you old?',
-    'Where are you frome?'
-  ];
+let count = 5;
 
-
-for (let i = 0; i < questions.length; i++) {
-  answer[i] = prompt(questions[i], '');
+function humanSayHello(user) {
+  
+  console.log(count);
+  console.log('Hello ' + user + '!');
 }
 
-document.write(answer);
-console.log("answer", answer);
+humanSayHello();
+console.log(count);
+
+// function Declaration
+
+let calc = function(a, b) {
+  return a + b;
+};
+
+
+console.log(calc(5,4));
+console.log(calc(8,1));
+console.log(calc(2,4));
+console.log(calc(2,3));
