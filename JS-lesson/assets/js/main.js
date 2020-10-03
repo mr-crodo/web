1b919ecdc3fc541b0c1f735fc984ae77e7edc987
Nasib <bduti09@gmail.com>
2020-10-04 01:00:13 +0400

    My first litle program on Js

--- JS-lesson/assets/js/main.js
+++ JS-lesson/assets/js/main.js
@@ -1,25 +1,101 @@
-"use strict";
+// // Comment
+// // "use strict";
 
 
-let count = 5;
+// let answer = [],
+//   questions = [
+//     'What is you name?',
+//     'What is you sure name?',
+//     'What is you old?',
+//     'Where are you frome?'
+//   ];
 
-function humanSayHello(user) {
-  
-  console.log(count);
-  console.log('Hello ' + user + '!');
+
+// for (let i = 0; i < questions.length; i++) {
+//   answer[i] = prompt(questions[i], '');
+// }
+
+// document.write(answer);
+// console.log("answer", answer);
+
+
+
+// "use strict";
+
+
+// let age = prompt("Сколько вам лет?", "");
+
+// if (age < 18) {
+//   alert('Ranovato esho!!');
+// } else if (age > 100) {
+//   alert('Pozdnovato esho');
+// } else {
+//   alert('Vi mojete')
+// }
+// // *****************
+
+// let count = 5;
+
+// function humanSayHello(user) {
+
+//   console.log(count);
+//   console.log('Hello ' + user + '!');
+// }
+
+// humanSayHello();
+// console.log(count);
+
+// // function Declaration
+
+// let calc = function (a, b) {
+//   return a + b;
+// };
+
+
+// console.log(calc(5, 4));
+// console.log(calc(8, 1));
+// console.log(calc(2, 4));
+// console.log(calc(2, 3));
+
+
+function myFirstApp(name, age) {
+  alert(`Привет, меня зовут ${name} и это моя первая программа!`);
+
+  function showSkills() {
+    let skills = ['html', 'css', 'bootstrap', 'js'];
+
+    for (let i = 0; i < skills.length; i++) {
+      // skills[i] = prompt(skills[i], '');
+      document.write(' Я владею ' + ' ' + skills[i]);
+      
+    }
+  }
+
+  showSkills();
+
+  function chechAge() {
+    if(age >= 18 && age < 25) {
+      alert('У тебя отличные шансы стать фронтенд разработчиком');
+    } else if( age >=25) {
+      alert('Еще не все потерянно бейся до конца!!!!');
+    } else {
+      alert('У тебя уровень Марка Цукербера');
+    }
+  }
+
+  chechAge();
+
+   function calcPow(num) {
+    num = prompt('введите знасения num ', '');
+    // num**
+   document.write(' ' + num * num);
+   }
+
+   calcPow();
+
+
 }
 
-humanSayHello();
-console.log(count);
-
-// function Declaration
-
-let calc = function(a, b) {
-  return a + b;
-};
-
-
-console.log(calc(5,4));
-console.log(calc(8,1));
-console.log(calc(2,4));
-console.log(calc(2,3));
+name = prompt(' Введите имя ');
+age = prompt('Введите возраст');
+myFirstApp(name, age);
