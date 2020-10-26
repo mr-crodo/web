3eeb5bfc6effc67820ef70ef6c834072b9c0d7d9
Nasib <bduti09@gmail.com>
2020-10-26 20:46:39 +0400

    R

--- JS-lesson/assets/js/script1.js
+++ JS-lesson/assets/js/script1.js
@@ -16,3 +16,20 @@ for(i = 30; i <=30 && i >=1 ; i--) {
     console.log(i);
   }
 }
+
+
+var manLive = function manLive(male, maleName){
+  switch(male) {
+    case "Kavkazian":
+      return maleName + " army.";
+    case "France":
+      return maleName + " eat lyagushko";
+    case "Turkey":
+      return maleName + " upravlaet Bezpilotnik Bayragdar";
+    case "Azerbayjani":
+      return maleName + " drive Armenian soldiers from the field like dogs.";
+  }
+   
+};
+
+console.log(manLive('Turkey' , 'Abdul'));
