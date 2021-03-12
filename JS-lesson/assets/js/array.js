23903ebe9e4f500c90ce98559376052a2b14cd2f
Nasib <bduti09@gmail.com>
2021-03-13 03:50:01 +0400

    array

--- JS-lesson/assets/js/array.js
+++ JS-lesson/assets/js/array.js
@@ -1,22 +1,22 @@
-let
-  color1 = "red",
-  color2 = "orange",
-  color3 = "yellow",
-  color4 = "green",
-  color5 = "blue",
-  color6 = "indigo",
-  color7 = "violet";
+// let
+//   color1 = "red",
+//   color2 = "orange",
+//   color3 = "yellow",
+//   color4 = "green",
+//   color5 = "blue",
+//   color6 = "indigo",
+//   color7 = "violet";
 
-  let rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+  // let rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-  console.log(rainbowColors[2]);
-  rainbowColors[0] = "Orange";
-  console.log(rainbowColors);
-  rainbowColors[7] = "red";
-  console.log(rainbowColors);
-  rainbowColors[rainbowColors.length] = "Olele";
-  console.log(rainbowColors);
-  let emptyArray = [];
-  let eptyArrayNew = new Array();
+  // console.log(rainbowColors[2]);
+  // rainbowColors[0] = "Orange";
+  // console.log(rainbowColors);
+  // rainbowColors[7] = "red";
+  // console.log(rainbowColors);
+  // rainbowColors[rainbowColors.length] = "Olele";
+  // console.log(rainbowColors);
+  // let emptyArray = [];
+  // let eptyArrayNew = new Array();
 
   document.body.style.backgroundColor = prompt('background color?', 'green');
--- JS-lesson/assets/js/function.js
+++ JS-lesson/assets/js/function.js
@@ -1,23 +1,78 @@
-function coplExe(helloText, name, age) {
-    console.log(helloText + " " + "My name is" + " " + name + " " + "my vozrast " + age)
-}
+// function coplExe(helloText, name, age) {
+//     console.log(helloText + " " + "My name is" + " " + name + " " + "my vozrast " + age)
+// }
 
-function square(number) {
-    return number * number;
-}
+// function square(number) {
+//     return number * number;
+// }
 
-let x = square(3);
+// let x = square(3);
+// console.log(x);
+
+// coplExe("Hi", "Nasib", 27);
+
+// function isSquareBig(side) {
+//     var squareArea = square(side);
+//     if(squareArea > 100) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(isSquareBig(200));
+
+// function animalVoice(animal, animalName) {
+//     switch(animal) {
+//         case "dog":
+//             return animalName + " barks.";
+//         case "cate":
+//             return animalName + " meows";
+//         case "pig":
+//             return animalName + " grunts";
+//         default:
+//             return animalName + " makes some sounds";
+//     }
+// }
+
+// console.log(animalVoice("dog", "Richard"))
+
+// let animalVoice = function(animal, animalName) {
+//     switch(animal) {
+//                 case "dog":
+//                     return animalName + " barks.";
+//                 case "cate":
+//                     return animalName + " meows";
+//                 case "pig":
+//                     return animalName + " grunts";
+//                 default:
+//                     return animalName + " makes some sounds";
+//             }
+// };
+
+// console.log(animalVoice("cat", "Richard"));
+
+// animalVoice = 20;
+
+// console.log(animalVoice("cat", "Richard"));
+
+// let numberOdd = function(e) {
+//     if (e % 2 == 0) {
+//         console.log(false);
+//     } else {
+//         console.log(true);
+//     }
+// }
+// numberOdd(5);
+// alert(numberOdd(20));
+
+function someFunction() {
+    let x = 1;
+    console.log(x);
+    console.log(y);
+    y++;
+}
+let y = 8;
+someFunction();
+
 console.log(x);
-
-coplExe("Hi", "Nasib", 27);
-
-function isSquareBig(side) {
-    var squareArea = square(side);
-    if(squareArea > 100) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-console.log(isSquareBig(200));
