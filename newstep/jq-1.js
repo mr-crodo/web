911d597188b6b6e9070a112ca39ab06b348a2f55
Nasib <bduti09@gmail.com>
2021-05-11 00:21:40 +0400

    fixed

---
+++ newstep/jq-1.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//             $("h2").click(function() {
+//               $(this).css("background-color", "#17efd2");
+//             });
+//           });
+
+$(document).ready(function() {
+  $("#blue").click(function() {
+    $(this).css("background", "blue");
+  })
+})
+
+$("#blue").css("cursor", "pointer");
---
+++ newstep/main.js
@@ -0,0 +1,25 @@
+// let 
+//   a = document.querySelector('a');
+
+//   let oldHref = a.getAttribute('href');
+
+//   nes = 5;
+
+//   console.log(nes);
+//   document.querySelector('a').innerHTML
+
+
+
+//   a.setAttribute('title', 'Necesen ay Gaa ne var ne yox');
+
+// let admin, naMe; // можно объявить две переменные через запятую
+
+// naMe = "Джон";
+
+// admin = naMe;
+
+// alert(admin); // "Джон"
+
+let user = prompt('Какое «официальное» название JavaScript?', '');
+
+(user === "ECMAScript") ? alert('Правильно!') : alert("Не знаете? “ECMAScript”!");
