3804324e1fd99dc4a24fec026862fd0b44f976b5
Nasib <bduti09@gmail.com>
2021-05-14 02:12:13 +0400

    fixed

---
+++ vue-project/src/Actions.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Actions"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue-project/src/SecondCounter.vue
+++ vue-project/src/SecondCounter.vue
@@ -1,10 +1,10 @@
 <template>
-    <h1>Home page</h1>
+$END$
 </template>
 
 <script>
 export default {
-  name: "Home"
+name: "SecondCounter"
 }
 </script>
 
